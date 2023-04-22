50134730a3b89977fed416323548a4d7b40d3c93
RicardoRG73 <ricardo.roman@umich.mx>
2023-04-21 18:08:24 -0600

    dominio 1D added

--- 01-poisson1D-Dirichlet.py
+++ 01-poisson1D-Dirichlet.py
@@ -31,7 +31,7 @@ Ktemp = np.diag(2*np.ones(N-2)) \
 K[1:-1, 1:-1] = Ktemp
 K[0,0] = -1
 K[-1,-1] = -1
-print(K)
+print("\n\n\nK =\n", K)
 K = K / h**2
 
 F = f(x)
@@ -39,24 +39,17 @@ F[1] += alpha / h**2
 F[-2] += beta / h**2
 F[0] = alpha / h**2
 F[-1] = beta / h**2
-print(F)
+print("F =\n",F)
 
 """ Solución del sistema KU = F """
 U = np.linalg.solve(K,F)
 
 """ Gáficas """
-print(plt.style.available)
+# print(plt.style.available)
 plt.style.use(['seaborn-v0_8','paper.mplstyle'])
 plt.rcParams['text.usetex'] = True
 
-fig1 = plt.figure(figsize=(8,4))
-plt.plot(x,0*x, marker='o')
-plt.text(x=x[N//2], y=0.1, s=r"$x_i$")
-plt.axis("equal")
-plt.title("Nodos")
-
-
-fig2 = plt.figure(figsize=(8,4))
+fig = plt.figure(figsize=(8,4))
 plt.plot(x,U, marker='o')
 plt.axis("equal")
 plt.title("Solución")
