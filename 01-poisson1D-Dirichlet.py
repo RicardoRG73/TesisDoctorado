5a29f82484cabc522b35fb4cbbcf9117bc3dcdc7
RicardoRG73 <ricardo.roman@umich.mx>
2023-04-25 13:15:53 -0600

    ejemplos de poisson corregidos

--- 01-poisson1D-Dirichlet.py
+++ 01-poisson1D-Dirichlet.py
@@ -25,19 +25,19 @@ beta = 0
 
 """ Ensamble del sistema KU = F """
 K = np.zeros((N,N))
-Ktemp = np.diag(2*np.ones(N-2)) \
-    + np.diag(-1*np.ones(N-3), k=1) \
-    + np.diag(-1*np.ones(N-3), k=-1)
+Ktemp = np.diag(-2*np.ones(N-2)) \
+    + np.diag(1*np.ones(N-3), k=1) \
+    + np.diag(1*np.ones(N-3), k=-1)
 K[1:-1, 1:-1] = Ktemp
-K[0,0] = -1
-K[-1,-1] = -1
+K[0,0] = 1
+K[-1,-1] = 1
 print("\n\n\nK =\n", K)
 K = K / h**2
 
 F = f(x)
-F[1] += alpha / h**2
-F[-2] += beta / h**2
 F[0] = alpha / h**2
+F[1] -= alpha / h**2
+F[-2] -= beta / h**2
 F[-1] = beta / h**2
 print("F =\n",F)
 
@@ -45,13 +45,15 @@ print("F =\n",F)
 U = np.linalg.solve(K,F)
 
 """ Gáficas """
-# print(plt.style.available)
 plt.style.use(['seaborn-v0_8','paper.mplstyle'])
-plt.rcParams['text.usetex'] = True
 
 fig = plt.figure(figsize=(7,3))
-plt.plot(x,U, marker='o')
+plt.plot(x,U, marker='o', label="Numérica")
 plt.axis("equal")
 plt.title("Solución")
+
+# exacta
+plt.plot(x, 0.5*(x**2 - x), label=r"Exacta $\frac{1}{2}(x^2-x)$")
+plt.legend()
 
 plt.show()
--- 02b-poisson1D-Neumann.py
+++ 02b-poisson1D-Neumann.py
@@ -19,19 +19,19 @@ x = np.linspace(0,1,N)
 h = x[1] - x[0]
 
 """ Parámetros del problema """
-f = lambda x: x
+f = lambda x: 0*x + 1
 alpha = 0
 beta = 0
 
 """ Ensamble del sistema KU = F """
 K = np.zeros((N,N))
-Ktemp = np.diag(2*np.ones(N-2)) \
-    + np.diag(-1*np.ones(N-3), k=1) \
-    + np.diag(-1*np.ones(N-3), k=-1)
+Ktemp = np.diag(-2*np.ones(N-2)) \
+    + np.diag(1*np.ones(N-3), k=1) \
+    + np.diag(1*np.ones(N-3), k=-1)
 K[1:-1, 1:-1] = Ktemp
-K[0,0] = -1
-K[-1,-1] = -1
-K[-2,-1] = -1
+K[0,0] = 1
+K[-1,-1] = -2
+K[-2,-1] = 1
 K[-1,-2] = 2
 print("\n\n\nK =\n", K)
 K = K / h**2
@@ -46,13 +46,16 @@ print("F =\n",F)
 U = np.linalg.solve(K,F)
 
 """ Gáficas """
-# print(plt.style.available)
 plt.style.use(['seaborn-v0_8','paper.mplstyle'])
-plt.rcParams['text.usetex'] = True
 
 fig = plt.figure(figsize=(7,3))
-plt.plot(x,U, marker='o')
+plt.plot(x,U, marker='o', label="Numérica")
 plt.axis("equal")
 plt.title("Solución")
+
+
+# Solución exacta
+plt.plot(x,x**2/2-x, label=r"Exacta $u=\frac{x^2}{2}-x$", lw=1)
+plt.legend()
 
 plt.show()
