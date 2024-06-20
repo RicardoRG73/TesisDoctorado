c39e1be390bb5f145f8278e39502b10936c38053
RicardoRG73 <ricardo.roman@umich.mx>
2024-06-20 09:44:59 -0600

    lineal stability BE and CN

--- A-stability-lineal.py
+++ A-stability-lineal.py
@@ -9,6 +9,9 @@ Created on Tue Jun 18 13:23:58 2024
 import numpy as np
 import matplotlib.pyplot as plt
 
+# =============================================================================
+# Forward Euler
+# =============================================================================
 # complex plane discretization
 N = 301
 limits_x = [-3, 1]
@@ -17,8 +20,10 @@ x = np.linspace(limits_x[0], limits_x[1], N)
 y = np.linspace(limits_y[0], limits_y[1], N)
 x,y = np.meshgrid(x,y)
 
-# condition Euler |1+z| <= 1
-condition = np.sqrt( (1 + x)**2 + y**2 )
+z = x + 1j*y    # complex plane
+
+# condition |1+z| <= 1
+condition = np.abs(1+z)
 condition = condition <= 1
 condition = condition * 1
 
@@ -31,4 +36,67 @@ plt.colorbar()
 plt.xlim(limits_x)
 plt.ylim(limits_y)
 plt.axis("equal")
+plt.title("Forward Euler")
+plt.show()
+
+
+# =============================================================================
+# Backward Euler
+# =============================================================================
+# complex plane discretization
+N = 301
+limits_x = [-1, 3]
+limits_y = [-2, 2]
+x = np.linspace(limits_x[0], limits_x[1], N)
+y = np.linspace(limits_y[0], limits_y[1], N)
+x,y = np.meshgrid(x,y)
+
+z = x + 1j*y    # complex plane
+
+# condition |1/(1-z)| <= 1
+condition = np.abs( 1 / (1-z) )
+condition = condition <= 1
+condition = condition * 1
+
+# figure
+plt.figure()
+plt.contourf(x,y,condition, levels=[-1,0,1], cmap="Blues")
+plt.axhline(0, color="k")
+plt.axvline(0, color="k")
+plt.colorbar()
+plt.xlim(limits_x)
+plt.ylim(limits_y)
+plt.axis("equal")
+plt.title("Backward Euler")
+plt.show()
+
+
+# =============================================================================
+# Crank-Nicolson
+# =============================================================================
+# complex plane discretization
+N = 301
+limits_x = [-2, 2]
+limits_y = [-2, 2]
+x = np.linspace(limits_x[0], limits_x[1], N)
+y = np.linspace(limits_y[0], limits_y[1], N)
+x,y = np.meshgrid(x,y)
+
+z = x + 1j*y    # complex plane
+
+# condition | (1 + z/2) / (1 - z/2) | <= 1
+condition = np.abs( (1 + z/2) / (1 - z/2) )
+condition = condition <= 1
+condition = condition * 1
+
+# figure
+plt.figure()
+plt.contourf(x,y,condition, levels=[-1,0,1], cmap="Blues")
+plt.axhline(0, color="k")
+plt.axvline(0, color="k")
+plt.colorbar()
+plt.xlim(limits_x)
+plt.ylim(limits_y)
+plt.axis("equal")
+plt.title("Crank-Nicolson")
 plt.show()
