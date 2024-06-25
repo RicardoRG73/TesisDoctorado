4777a6a2ba3f8dea9b381e71845b274d88ea3a16
RicardoRG73 <ricardo.roman@umich.mx>
2024-06-24 20:20:23 -0600

    stability regions RKF45

--- A-stability-lineal.py
+++ A-stability-lineal.py
@@ -5,16 +5,18 @@ Created on Tue Jun 18 13:23:58 2024
 
 @author: ricardo
 """
+save_figures = False
 # libraries
 import numpy as np
 import matplotlib.pyplot as plt
+plt.style.use(["seaborn-v0_8"])
 
 # =============================================================================
 # Forward Euler
 # =============================================================================
 # complex plane discretization
 N = 301
-limits_x = [-3, 1]
+limits_x = [-3.5, 1.5]
 limits_y = [-2, 2]
 x = np.linspace(limits_x[0], limits_x[1], N)
 y = np.linspace(limits_y[0], limits_y[1], N)
@@ -37,6 +39,8 @@ plt.xlim(limits_x)
 plt.ylim(limits_y)
 plt.axis("equal")
 plt.title("Forward Euler")
+if save_figures:
+    plt.savefig("figuras/A-Stability-FE.pdf")
 plt.show()
 
 
@@ -45,7 +49,7 @@ plt.show()
 # =============================================================================
 # complex plane discretization
 N = 301
-limits_x = [-1, 3]
+limits_x = [-1.5, 3.5]
 limits_y = [-2, 2]
 x = np.linspace(limits_x[0], limits_x[1], N)
 y = np.linspace(limits_y[0], limits_y[1], N)
@@ -68,6 +72,8 @@ plt.xlim(limits_x)
 plt.ylim(limits_y)
 plt.axis("equal")
 plt.title("Backward Euler")
+if save_figures:
+    plt.savefig("figuras/A-Stability-BE.pdf")
 plt.show()
 
 
@@ -76,7 +82,7 @@ plt.show()
 # =============================================================================
 # complex plane discretization
 N = 301
-limits_x = [-2, 2]
+limits_x = [-2.5, 2.5]
 limits_y = [-2, 2]
 x = np.linspace(limits_x[0], limits_x[1], N)
 y = np.linspace(limits_y[0], limits_y[1], N)
@@ -99,4 +105,107 @@ plt.xlim(limits_x)
 plt.ylim(limits_y)
 plt.axis("equal")
 plt.title("Crank-Nicolson")
+if save_figures:
+    plt.savefig("figuras/A-Stability-CN.pdf")
+plt.show()
+
+
+# =============================================================================
+# RK2
+# =============================================================================
+# complex plane discretization
+N = 301
+limits_x = [-3.5, 1.5]
+limits_y = [-2, 2]
+x = np.linspace(limits_x[0], limits_x[1], N)
+y = np.linspace(limits_y[0], limits_y[1], N)
+x,y = np.meshgrid(x,y)
+
+z = x + 1j*y    # complex plane
+
+# condition
+condition = 1 + z + (z**2)/2
+condition = np.abs(condition) <= 1
+condition = condition * 1
+
+# figure
+plt.figure()
+plt.contourf(x,y,condition, levels=[-1,0,1], cmap="Blues")
+plt.axhline(0, color="k")
+plt.axvline(0, color="k")
+plt.colorbar()
+plt.xlim(limits_x)
+plt.ylim(limits_y)
+plt.axis("equal")
+plt.title("Runge-Kutta 2")
+if save_figures:
+    plt.savefig("figuras/A-Stability-RK2.pdf")
+plt.show()
+
+
+# =============================================================================
+# RKF45
+# =============================================================================
+# complex plane discretization
+N = 1001
+limits_x = [-7, 5.5]
+limits_y = [-4, 4]
+x = np.linspace(limits_x[0], limits_x[1], N)
+y = np.linspace(limits_y[0], limits_y[1], N)
+x,y = np.meshgrid(x,y)
+
+z = x + 1j*y    # complex plane
+
+# condition
+# condition = z * ( 0.00048*z**5 - 0.00128*z**4 )
+
+# RK5
+cond_RK5 = np.array([
+    0.000480769230769231*z**6,
+    0.00833333333333333*z**5,
+    0.0416666666666667*z**4,
+    0.166666666666667*z**3,
+    0.5*z**2,
+    z,
+    0*z + 1
+])
+cond_RK5 = np.sum(cond_RK5, axis=0)
+cond_RK5 = np.abs(cond_RK5) <= 1
+cond_RK5 = cond_RK5 * 1
+
+# RK4
+cond_RK4 = np.array([
+    0.00961538461538462*z**5,
+    0.0416666666666667*z**4,
+    0.166666666666667*z**3,
+    0.5*z**2,
+    z,
+    0*z + 1
+])
+cond_RK4 = np.sum(cond_RK4, axis=0)
+cond_RK4 = np.abs(cond_RK4) <= 1
+cond_RK4 = cond_RK4 * 1
+
+# figure
+plt.figure()
+contrk5 = plt.contourf(x,y,cond_RK5, levels=[-1,0,1], alpha=0.5,cmap="Reds")
+contrk4 = plt.contourf(x,y,cond_RK4, levels=[-1,0,1], alpha=0.5, cmap="Blues")
+
+coords = contrk5.allsegs[1][0]
+plt.plot(coords[:,0],coords[:,1], color="red", label="RK5")
+coords = contrk4.allsegs[1][0]
+plt.plot(coords[:,0],coords[:,1], color="blue", label="RK4")
+
+# plt.contourf(x,y,cond_RK4*cond_RK5, levels=[-1,0,1], alpha=0.5, cmap="Greys")
+
+
+plt.axhline(0, color="k")
+plt.axvline(0, color="k")
+plt.xlim(limits_x)
+plt.ylim(limits_y)
+plt.axis("equal")
+plt.title("Runge-Kutta-Fehlberg")
+plt.legend()
+if save_figures:
+    plt.savefig("figuras/A-Stability-RKF45.pdf")
 plt.show()
